6d8af4b9d755765dabcab5f777cb1cbaf57130c8
tuannguyenh1 <tuannguyenh1@nashtechglobal.com>
2025-01-09 08:27:51 +0700

    45

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,25 +1,28 @@
-resource "azurerm_resource_group" "aks-rg" {
+# resource "azurerm_resource_group" "aks-rg" {
+#   name     = var.resource_group_name
+#   location = var.location
+# }
+data "azurerm_resource_group" "aks-rg" {
   name     = var.resource_group_name
-  location = var.location
 }
 
-# resource "azurerm_role_assignment" "role_acrpull_backend" {
-#   scope                            = azurerm_container_registry.acr_backend.id
-#   role_definition_name             = "AcrPull"
-#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "role_acrpull_backend" {
+  scope                            = azurerm_container_registry.acr_backend.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
 
-# resource "azurerm_role_assignment" "role_acrpull_frontend" {
-#   scope                            = azurerm_container_registry.acr_frontend.id
-#   role_definition_name             = "AcrPull"
-#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-#   skip_service_principal_aad_check = true
-# }
+resource "azurerm_role_assignment" "role_acrpull_frontend" {
+  scope                            = azurerm_container_registry.acr_frontend.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
 
 resource "azurerm_container_registry" "acr_backend" {
   name                = var.acr_backend_name
-  resource_group_name = azurerm_resource_group.aks-rg.name
+  resource_group_name = data.azurerm_resource_group.aks-rg.name
   location            = var.location
   sku                 = "Standard"
   admin_enabled       = false
@@ -27,7 +30,7 @@ resource "azurerm_container_registry" "acr_backend" {
 
 resource "azurerm_container_registry" "acr_frontend" {
   name                = var.acr_frontend_name
-  resource_group_name = azurerm_resource_group.aks-rg.name
+  resource_group_name = data.azurerm_resource_group.aks-rg.name
   location            = var.location
   sku                 = "Standard"
   admin_enabled       = false
@@ -37,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
   kubernetes_version  = var.kubernetes_version
   location            = var.location
-  resource_group_name = azurerm_resource_group.aks-rg.name
+  resource_group_name = data.azurerm_resource_group.aks-rg.name
   dns_prefix          = var.cluster_name
 
   default_node_pool {
--- infra/terraform/terraform.tfvars
+++ infra/terraform/terraform.tfvars
@@ -1,7 +1,7 @@
-resource_group_name = "msa_aks_tf_rg"
+resource_group_name = "tuannguyen"
 location            = "SoutheastAsia"
 cluster_name        = "msa-aks-cluster"
 kubernetes_version  = "1.30.6"
 system_node_count   = 2
-acr_backend_name    = "msabackend"
-acr_frontend_name   = "msafrontend"
+acr_backend_name    = "msabackend1"
+acr_frontend_name   = "msafrontend1"
